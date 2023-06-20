42951cef9c53293137c547d4517da5aa6290186b
Muhammad Maaz Ali <muhammadmaazali112@gmail.com>
2023-06-20 17:32:25 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SimpleTableComponent } from './simple-table/simple-table.component';
+import { UserService } from './table.service';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, SimpleTableComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [UserService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/simple-table/simple-table.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../table.service';
+
+@Component({
+  selector: 'app-simple-table',
+  templateUrl: './simple-table.component.html',
+  styleUrls: ['./simple-table.component.css'],
+})
+export class SimpleTableComponent implements OnInit {
+  users: any[];
+  sortedColumn: string;
+  sortDirection: string;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.userService.getUsers().subscribe(
+      (users: any[]) => {
+        this.users = users;
+      },
+      (error) => {
+        console.log('Error fetching users:', error);
+      }
+    );
+  }
+
+  sort(column: string) {
+    if (column === this.sortedColumn) {
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.sortedColumn = column;
+      this.sortDirection = 'asc';
+    }
+
+    this.users.sort((a, b) => {
+      const valA = this.getNestedValue(a, column);
+      const valB = this.getNestedValue(b, column);
+
+      if (valA < valB) {
+        return this.sortDirection === 'asc' ? -1 : 1;
+      } else if (valA > valB) {
+        return this.sortDirection === 'asc' ? 1 : -1;
+      } else {
+        return 0;
+      }
+    });
+  }
+
+  getNestedValue(obj: any, path: string) {
+    const parts = path.split('.');
+    let value = obj;
+
+    for (const part of parts) {
+      if (value[part] === undefined) {
+        return null;
+      }
+      value = value[part];
+    }
+
+    return value;
+  }
+}
